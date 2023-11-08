ebb8dcd82b3828f08a0b646ec64a9bd837afd4ca
damianmiskow <111329721+damianmiskow@users.noreply.github.com>
2023-11-07 20:58:01 -0500

    setting up components, and homepage css

---
+++ src/components/Dashboard/EditTemplates.js
@@ -0,0 +1,18 @@
+// src/components/Dashboard/NewInterview.js
+import React from 'react';
+
+const NewInterview = () => {
+  // Preloaded templates can be fetched or imported
+
+  return (
+    <div>
+      <h2>New Edit Templates</h2>
+      {/* Display templates here */}
+    </div>
+  );
+};
+
+export default NewInterview;
+
+
+//DELTE ALL OF THISCODE START FROM SCRATCH
