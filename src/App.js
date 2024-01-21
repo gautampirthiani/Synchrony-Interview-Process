f8af46f088e0e9ed1054d57f28655bb8a6325a75
gautampirthiani <118488072+gautampirthiani@users.noreply.github.com>
2024-01-21 18:35:58 -0500

    Adding basic login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 // src/App.js
 import React from 'react';
+import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import Interviews from './components/Dashboard/Interviews';
 import NewInterview from './components/Dashboard/NewInterview';
+import Login from './components/Login';
 
 import './App.css';
 
 function App() {
+  const [token, setToken] = useState();
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
   return (
     <Router>
       <Routes>
