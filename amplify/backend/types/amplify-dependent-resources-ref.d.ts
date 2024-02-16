66f14a39c68bb8b31b99650e67965d6c9376c808
damianmiskow <111329721+damianmiskow@users.noreply.github.com>
2024-02-15 19:17:43 -0500

    gitignore update

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
   "auth": {
-    "synchronyinterviewprb33e3b29": {
+    "synchronyinterviewprbfa7c454": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
       "IdentityPoolId": "string",
