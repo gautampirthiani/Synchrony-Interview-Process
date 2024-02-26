2c5d80979ea1b080e96e0279f2a1492e33c5b4dd
damianmiskow <111329721+damianmiskow@users.noreply.github.com>
2024-02-26 12:50:43 -0500

    working change page based on user

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logoimage from './synchrony-logo-1.png';
 import './HomePage.css';
 import Navbar from './Navbar';
+import { getCurrentUser } from '@aws-amplify/auth';
 
 const HomePage = () => {
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    getCurrentUser()
+      .then(user => {
+        setUsername(user.username);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
   return (
     <div className="homepage-container">
       <div className="logo-container">
@@ -15,7 +26,9 @@ const HomePage = () => {
         <Navbar />
       </div>
 
-      <h1 className="homepage-heading">Synchrony Interviews</h1>
+      <h1 className="homepage-heading">Welcome, {username}!</h1>
+
+      <h2 className="homepage-heading">Synchrony Interviews</h2>
 
       <footer className="footer">
         <div className="footer-content">
