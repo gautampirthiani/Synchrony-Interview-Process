ebf4a66c9473f2ca53b23e9961c1d663f102f82e
damianmiskow <111329721+damianmiskow@users.noreply.github.com>
2024-03-20 13:08:39 -0400

    a

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 // src/App.js
 import React from 'react';
-//import { useState } from 'react';   // commented because not used
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import HomePage from './components/HomePage';
 import Interviews from './components/Dashboard/Interviews';
 import NewInterview from './components/Dashboard/NewInterview';
@@ -14,27 +15,36 @@ import UpdateTemplates from './components/EditTemplates/UpdateTemplates';
 import ConductInterview from './components/NewInterview/ConductInterview';
 import JobInterviews from './components/Interviews/JobInterviews';
 
-
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/dashboard/interviews" element={<Interviews />} />
-        <Route path="/dashboard/New-interview" element={<NewInterview />} />
-        <Route path="/dashboard/edit-templates" element={<EditTemplates />} />
-        <Route path="/interview-details/:interviewId" element={<InterviewDetails />} />
-        <Route path="/new-interview/conduct-interview/:jobId" element={<ConductInterview />} />
-        <Route path="/dashboard/new-templates" element = {<NewTemplates />} />
-        <Route path="/dashboard/data-analysis" element = {<DataAnalysis />} />
-        <Route path="/dashboard/templates/:JobID" element={<Templates />} />
-        <Route path="/dashboard/update-templates/:jobId/:templateId" element={<UpdateTemplates />} /> 
-        <Route path="/dashboard/new-templates/:jobId" element={<NewTemplates />} />
-        <Route path="/interviews/job-interviews/:jobId/:jobPosition" element={<JobInterviews />} />
-      </Routes>
-    </Router>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <Router>
+          <div className="auth-wrapper">
+            <div className="top-bar">
+              <span className="welcome-message">Welcome, {user?.username}</span>
+              <button onClick={signOut} className="sign-out-button">Sign Out</button>
+            </div>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/dashboard/interviews" element={<Interviews />} />
+              <Route path="/dashboard/New-interview" element={<NewInterview />} />
+              <Route path="/dashboard/edit-templates" element={<EditTemplates />} />
+              <Route path="/interview-details/:interviewId" element={<InterviewDetails />} />
+              <Route path="/new-interview/conduct-interview/:jobId" element={<ConductInterview />} />
+              <Route path="/dashboard/new-templates" element={<NewTemplates />} />
+              <Route path="/dashboard/data-analysis" element={<DataAnalysis />} />
+              <Route path="/dashboard/templates/:JobID" element={<Templates />} />
+              <Route path="/dashboard/update-templates/:jobId/:templateId" element={<UpdateTemplates />} />
+              <Route path="/dashboard/new-templates/:jobId" element={<NewTemplates />} />
+              <Route path="/interviews/job-interviews/:jobId/:jobPosition" element={<JobInterviews />} />
+            </Routes>
+          </div>
+        </Router>
+      )}
+    </Authenticator>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import awsExports from './aws-exports';
+import { Amplify } from 'aws-amplify';
+Amplify.configure(awsExports);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
