4777373945012cc64def69805ae791cd5b12d212
LiyangWan <146778153+LiyangWan@users.noreply.github.com>
2024-04-14 18:57:58 -0400

    Merged Emailing transcript function and dynamic textbox
    Merged Emailing transcript function and dynamic textbox in conductinterview.css and js

--- src/components/NewInterview/ConductInterview.js
+++ src/components/NewInterview/ConductInterview.js
@@ -15,44 +15,36 @@ function ConductInterview() {
   const [username, setUsername] = useState('');
   const [templateName, setTemplateName] = useState('');
   const [jobPosition, setJobPosition] = useState('');
+  const [email, setEmail] = useState('');
+  const [showModal, setShowModal] = useState(false);
 
-  useEffect(() => {
-    getCurrentUser().then(user => {
-      setUsername(user.username);
-      if (jobId) {
-        axios.post('https://h60ydhn92g.execute-api.us-east-1.amazonaws.com/dev/GetDefaultTemplate', {
-          username: user.username,
-          jobID: jobId
-        })
-          .then(response => {
-            setTemplateId(response.data.templateID);
-          })
-          .catch(error => console.error('Error fetching default template:', error));
-      }
+  const autoGrow = (element) => {
+    document.querySelectorAll('.additional-inputs-container').forEach(container => {
+      var first_input = container.getElementsByClassName('additional-input')[0];
+      var second_input = container.getElementsByClassName('second-input')[0];
+  
+      // Reset the height to 'auto' before calculating the new height
+      // to allows the box to shrink if the content has been deleted
+      first_input.style.height = 'auto';
+      second_input.style.height = 'auto';
+  
+      let first_height = first_input.scrollHeight;
+      let second_height = second_input.scrollHeight;
+      
+      let greater_height = Math.max(first_height, second_height);
+      first_input.style.height = greater_height + 'px';
+      second_input.style.height = greater_height + 'px';
     });
-  }, [jobId]);
-
-  useEffect(() => {
-    fetchdata();
-    query_job_positon();
-  }, [jobId, templateId]);
-
-  // Query job positon
-  const query_job_positon = async () => {
-    try {
-      setLoading(true);
-      const response = await axios.get(`https://rv0femjg65.execute-api.us-east-1.amazonaws.com/default/get_jobPosition?jobId=${jobId}`);
-      // console.log("11")
-      // console.log(response.data)
-      setJobPosition(response.data)
-
-    } catch (error) {
-      // console.error('Error fetching data:', error);
-    }
-    finally {
-      setLoading(false);
-    }
   };
+
+  function updateAdditionalInputsFromMultiple(questions) {
+    const newItems = questions.map(q => ({
+      question: q.Question,
+      answer: q.Answer,
+      score: q.Score
+    }));
+    setAdditionalInputs(newItems);
+  }
 
   const fetchdata = async () => {
     if (templateId && jobId) {
@@ -74,14 +66,106 @@ function ConductInterview() {
     }
   };
 
-  function updateAdditionalInputsFromMultiple(questions) {
-    const newItems = questions.map(q => ({
-      question: q.Question,
-      answer: q.Answer,
-      score: q.Score
+
+  useEffect(() => {
+    getCurrentUser().then(user => {
+      setUsername(user.username);
+      if (jobId) {
+        axios.post('https://h60ydhn92g.execute-api.us-east-1.amazonaws.com/dev/GetDefaultTemplate', {
+          username: user.username,
+          jobID: jobId
+        })
+          .then(response => {
+            setTemplateId(response.data.templateID);
+          })
+          .catch(error => console.error('Error fetching default template:', error));
+      }
+    });
+    fetchdata();
+    query_job_positon();
+  }, [jobId, templateId]);
+
+  // Query job positon
+  const query_job_positon = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(`https://rv0femjg65.execute-api.us-east-1.amazonaws.com/default/get_jobPosition?jobId=${jobId}`);
+      // console.log("11")
+      // console.log(response.data)
+      setJobPosition(response.data)
+
+    } catch (error) {
+      // console.error('Error fetching data:', error);
+    }
+    finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    const questionsPayload = additionalInputs.map(({ question, answer, score }) => ({
+      QuestionText: question,
+      Answer: answer,
+      Score: score
     }));
-    setAdditionalInputs(newItems);
-  }
+
+    try {
+      const response = await axios.post(`https://rv0femjg65.execute-api.us-east-1.amazonaws.com/default/Submit-Interview?JobId=${jobId}`, {
+        Name: candidateName,
+        Questions: questionsPayload,
+        Interviewer: username // Include username in the request payload
+      });
+
+      alert('Interview submitted successfully!');
+      navigate(`/dashboard/new-interview`);
+    } catch (error) {
+      console.error('Error submitting new interview:', error);
+    }
+  };
+
+  const handleEmailSubmit = async (SendEmail) => {
+    if (window.confirm('Submit new interview?')) {
+      const questionsPayload = additionalInputs.map(({ question, answer, score }) => ({
+        QuestionText: question,
+        Answer: answer,
+        Score: score
+      }));
+    
+      if (SendEmail && email) {
+        try {
+          const emailResponse = await axios.post('https://h60ydhn92g.execute-api.us-east-1.amazonaws.com/dev/SendEmail', {
+            jobId: jobId,
+            email: email,
+            questions: questionsPayload
+          });
+          
+          console.log('Email response:', emailResponse);
+
+          // Check the response status code and the response data for a success message
+          if (emailResponse.status === 200 && emailResponse.data.message === 'Email sent successfully!') {
+            alert('Email sent successfully!');
+            handleSubmit();
+          } else {
+            alert('Received unexpected response: ' + JSON.stringify(emailResponse.data));
+          }
+        } catch (error) {
+            if (error.response) {
+              console.error('Error response:', error.response);
+              alert('Failed to send email: ' + error.response.data.message);
+            } else if (error.request) {
+              console.error('Error request:', error.request);
+              alert('Failed to send email, please check the email you submitted.');
+            } else {
+              console.error('Error message:', error.message);
+              alert('Failed to send email: ' + error.message);
+            }
+          }
+      } else {
+        // Submit the form if the email has been sent & chose not to sent
+        handleSubmit();
+      }
+    }
+  };
 
   const handleAdditionalInputChange = (index, key, value) => {
     setAdditionalInputs(inputs =>
@@ -95,30 +179,6 @@ function ConductInterview() {
 
   const removeInputPair = (index) => {
     setAdditionalInputs(inputs => inputs.filter((_, i) => i !== index));
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    if (window.confirm('Submit new interview?')) {
-      const questionsPayload = additionalInputs.map(({ question, answer, score }) => ({
-        QuestionText: question,
-        Answer: answer,
-        Score: score
-      }));
-
-      try {
-        const response = await axios.post(`https://rv0femjg65.execute-api.us-east-1.amazonaws.com/default/Submit-Interview?JobId=${jobId}`, {
-          Name: candidateName,
-          Questions: questionsPayload,
-          Interviewer: username // Include username in the request payload
-        });
-
-        alert('Interview submitted successfully!');
-        navigate(`/dashboard/new-interview`);
-      } catch (error) {
-        console.error('Error submitting new interview:', error);
-      }
-    }
   };
 
   return (
@@ -139,22 +199,26 @@ function ConductInterview() {
         />
       </div>
       <button id="add-question-answer-btn" onClick={addInputPair}>Add Question & Answer</button>
-      <button id="save-new-templates-btn" onClick={handleSubmit}>Submit</button>
+      <button id="save-new-templates-btn" onClick={() => setShowModal(true)}>Submit</button>
       {additionalInputs.map((input, index) => (
         <div key={index} className="additional-inputs-container">
-          <input
-            type="text"
+          <textarea
             placeholder="Question"
             value={input.question}
-            onChange={(e) => handleAdditionalInputChange(index, 'question', e.target.value)}
+            onChange={(e) => {
+              handleAdditionalInputChange(index, 'question', e.target.value);
+              autoGrow(e.target);
+            }}
             className="additional-input"
           />
-          <input
-            type="text"
+          <textarea
             placeholder="Answer"
             value={input.answer}
-            onChange={(e) => handleAdditionalInputChange(index, 'answer', e.target.value)}
-            className="additional-input"
+            onChange={(e) => {
+              handleAdditionalInputChange(index, 'answer', e.target.value);
+              autoGrow(e.target);
+            }}
+            className="second-input"
           />
           <input
             type="text"
@@ -166,6 +230,36 @@ function ConductInterview() {
           <button id="delete-btn" onClick={() => removeInputPair(index)}>Delete</button>
         </div>
       ))}
+      <EmailTranscriptModal
+        isOpen={showModal}
+        onClose={() => setShowModal(false)}
+        email={email}
+        setEmail={setEmail}
+        onSubmit={handleEmailSubmit}
+      />
+    </div>
+  );
+}
+
+function EmailTranscriptModal({ isOpen, onClose, email, setEmail, onSubmit }) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <span className="close" onClick={onClose}>&times;</span>
+        <h2>Would you like to email the transcript?</h2>
+        <input
+          type="email"
+          placeholder="Enter email address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <div className="modal-buttons">
+          <button onClick={() => onSubmit(true)}>Send Transcript & Submit</button>
+          <button onClick={() => onSubmit(false)}>Submit Without Sending</button>
+        </div>
+      </div>
     </div>
   );
 }
