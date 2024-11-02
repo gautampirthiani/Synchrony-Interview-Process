d99934d9ddebbd613e220e5151554158433e04be
gautampirthiani <118488072+gautampirthiani@users.noreply.github.com>
2024-11-02 11:43:21 -0400

    import /aws-exports commented out

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import { Amplify } from 'aws-amplify';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Attempt to import aws-exports.js
-try {
-    const awsExports = require('./aws-exports').default; // Use require to dynamically import
-    Amplify.configure(awsExports);
-} catch (error) {
-    console.warn('aws-exports.js not found, Amplify not configured:', error);
-    // You can configure default settings for Amplify here if necessary
-}
+// // Attempt to import aws-exports.js
+// try {
+//     const awsExports = require('./aws-exports').default; // Use require to dynamically import
+//     Amplify.configure(awsExports);
+// } catch (error) {
+//     console.warn('aws-exports.js not found, Amplify not configured:', error);
+//     // You can configure default settings for Amplify here if necessary
+// }
 
 root.render(
     <React.StrictMode>
